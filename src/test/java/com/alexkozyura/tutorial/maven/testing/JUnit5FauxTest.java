6c19f9e2bde47209cef18644dc1f827f934dc365
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-05-24 16:36:44 +1000

    Add junit5 test class

---
+++ src/test/java/com/alexkozyura/tutorial/maven/testing/JUnit5FauxTest.java
@@ -0,0 +1,11 @@
+package com.alexkozyura.tutorial.maven.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5FauxTest {
+
+    @Test
+    void someTestForJUnit5() {
+        System.out.println("I Ran...");
+    }
+}
