9109205668e023878b494faacd0b4d65c3e39bd3
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-05-26 13:49:50 +1000

    Add UserMapper

--- web-app/src/main/java/com/alexkozyura/tutorial/maven/web_app/controllers/UserController.java
+++ web-app/src/main/java/com/alexkozyura/tutorial/maven/web_app/controllers/UserController.java
@@ -1,11 +1,12 @@
 package com.alexkozyura.tutorial.maven.web_app.controllers;
 
+import com.alexkozyura.tutorial.maven.converters.UserMapper;
 import com.alexkozyura.tutorial.maven.entities.User;
 import com.alexkozyura.tutorial.maven.jaxb.model.UserCommand;
 
 public class UserController {
 
     User saveUser(UserCommand command) {
-        return new User();
+        return UserMapper.INSTANCE.userCommandToUser(command);
     }
 }
